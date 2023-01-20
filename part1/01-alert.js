eabba5760d4c14c3992ac06da17cdfa7d6c35942
p2635 <547001-pwrepo@users.noreply.gitlab.com>
2023-01-20 17:41:03 +0000

    tidy up code

--- part1/01-alert.js
+++ part1/01-alert.js
@@ -1,17 +1,10 @@
+/* Variables (https://javascript.info/hello-world)
+Date: 20 Jan 2023
+Author: Philip Wong
+*/
+
 'use strict'; // This enables new stuff, it can break old code.
 
-/* Multiple lines of comments here.
-Get ready to PARTAYYY.
-*/
 function showAlert(){
     alert( 'I\'m JavaScript!' );
 }
-
-// This is just a single line, so lonely.
-// (insert some code here please)
-
-/* When do I use CAPS vs camelCase for const?
-The answer is whether you know the value beforehand or not.
-const COLOUR_RED = "#F00";
-const pageLoadTime = /* time taken by a webpage to load
-*/
