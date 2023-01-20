a6a583fee4641f3f137a0c3ebeb531bfb0e6b94c
p2635 <547001-pwrepo@users.noreply.gitlab.com>
2023-01-20 17:17:51 +0000

    const notes

--- scripts/alert.js
+++ scripts/alert.js
@@ -9,3 +9,9 @@ function showAlert(){
 
 // This is just a single line, so lonely.
 // (insert some code here please)
+
+/* When do I use CAPS vs camelCase for const?
+The answer is whether you know the value beforehand or not.
+const COLOUR_RED = "#F00";
+const pageLoadTime = /* time taken by a webpage to load
+*/
