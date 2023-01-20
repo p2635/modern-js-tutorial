fa88d5ec3da94d02841cb5aead75debb03c05ecb
p2635 <547001-pwrepo@users.noreply.gitlab.com>
2023-01-20 17:06:19 +0000

    add comments

--- scripts/alert.js
+++ scripts/alert.js
@@ -1,3 +1,9 @@
+/* Multiple lines of comments here.
+Get ready to PARTAYYY.
+*/
 function showAlert(){
     alert( 'I\'m JavaScript!' );
 }
+
+// This is just a single line, so lonely.
+// (insert some code here please)
